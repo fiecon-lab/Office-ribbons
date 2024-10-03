531cad9f4bd553a8fe472ae76ec53c44c9d00b28
Tom Wright <tom.wright@fiecon.com>
2024-10-03 17:25:57 +0100

    Attempting to set up local testing

--- ppt-links/src/commands/commands.js
+++ ppt-links/src/commands/commands.js
@@ -1,54 +1,37 @@
+/* eslint-disable no-undef */
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
 
-
-
 Office.onReady(() => {
-    
   // Register the function with Office.
   Office.actions.associate("openCria", openCria);
   Office.actions.associate("findAbbrevs", findAbbrevs);
 });
 
-
 function openCria(event) {
-  window.open('https://cria.fiecon.com/', '_blank');
+  window.open("https://cria.fiecon.com/", "_blank");
   event.completed();
 }
 
-
 function findAbbrevs(event) {
-
-  console.log("Hello world");
-
-  // const message = {
-  //   type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-  //   message: "Performed action.",
-  //   icon: "Icon.80x80",
-  //   persistent: true,
-  // };
-
-  // // Show a notification message.
-  // Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
- 
-  // event.completed();
+    Office.context.document.setSelectedDataAsync("Coming soon!", function (asyncResult) {
+        if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+          console.error(asyncResult.error.message);
+        }
+      });
+      event.completed();
 }
-
 
 
 
 
 function helloWorld(event) {
-  Office.context.document.setSelectedDataAsync(
-      "Hello World!",
-      function (asyncResult) {
-          if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-              console.error(asyncResult.error.message);
-          }
-      }
-  );
+  Office.context.document.setSelectedDataAsync("Hello World this is a test!", function (asyncResult) {
+    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+      console.error(asyncResult.error.message);
+    }
+  });
   event.completed();
 }
-
